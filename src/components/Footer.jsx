e8c29db02a64812df3574360e8006214c25d7ca8
Pekamagit <160333069+Pekamagit@users.noreply.github.com>
2024-04-28 18:55:24 +0100

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full md:h-screen py-4 text-gray-300 bg-[#0a192f] text-center'>
+        <span> &copy; 2023 Paul all right reserved</span>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Work.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import Linkedin from '../assets/Linkedin.png'
+import crypto from '../assets/crypto.png'
+import todoList from '../assets/todoList.png'
+
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb=8'>
+                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-orange-600'>Work</p>
+                <p className='py-6'>Check out some of my recent work</p>
+            </div>
+
+          {/* Container */}
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+                {/* Grid Item */}
+                <div 
+                style={{backgroundImage: `url(${todoList})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                 {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+React JS Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='https://cool-unicorn-2b02a9.netlify.app/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='https://github.com/Pekamagit/react-todo-list.git'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div 
+                style={{backgroundImage: `url(${Linkedin})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                 {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+JS & CSS Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='https://pekamagit.github.io/LinkedIn-clone/profile.html'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='https://github.com/Pekamagit/LinkedIn-clone.git'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div 
+                style={{backgroundImage: `url(${crypto})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                 {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+React JS Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='matochcryptos.netlify.app'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='https://github.com/Pekamagit/react-tailwind-project.git'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+              
+
+
+
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Work
