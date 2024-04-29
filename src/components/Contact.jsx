8f5d94d61b4d451de17deb9bd1a0a20e120d8340
Pekamagit <160333069+Pekamagit@users.noreply.github.com>
2024-04-29 06:17:47 +0100

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,9 +7,9 @@ const Contact = () => {
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-orange-600 text-gray-300'>Contact</p>
 
-                <p className='text-gray-300 py-4'>// Submit the form below or send me an email - Ekamapaul86@gmail.com</p>
+                <p className='text-gray-300 py-4'>Submit the form below or send me an email - Ekamapaul86@gmail.com</p>
             </div>
-            <input className='bg-[#ccd6f6] P-2' type='text' placeholder='Name' name='name' />
+            <input className='bg-[#ccd6f6] P-2' type='text' autocomplete="off" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type='email' placeholder='Email' name='email' />
             <textarea className='bg-[#ccd6f6] p-2' name='message' rows={10} placeholder='Message'></textarea>
             <button className='text-white border-2 hover:bg-orange-600 hover:border-orange-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
