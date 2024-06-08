f70cd294d7a1b1407af5ea2ddafc6aedc199b514
Pekamagit <160333069+Pekamagit@users.noreply.github.com>
2024-06-08 19:34:13 +0100

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -40,6 +40,28 @@ React JS Application
                 <div 
                 style={{backgroundImage: `url(${Linkedin})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
+ {/* Hover Effects */}
+                 <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+React JS Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='https://weatherforcaster-app.netlify.app'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='https://pekamagit.github.io/React-Weather-App/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div 
+                style={{backgroundImage: `url(${Linkedin})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+
+                  
                  {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
